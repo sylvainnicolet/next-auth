6857c74dd8808adcf684c3e851585c54a8e0fac4
Sylvain Nicolet <nicoletsylvain@gmail.com>
2023-12-31 11:33:36 +0100

    Create login button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import { Button } from '@/components/ui/button'
+import { LoginButton } from '@/components/auth/login-button'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-center">🔐 Auth</h1>
+        <p className="text-center mt-2">A simple authentication app</p>
+        <LoginButton>
+          <Button size="lg" className="mt-4">
+            Sign in
+          </Button>
+        </LoginButton>
+      </div>
     </div>
   )
 }
