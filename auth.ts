44f16702c9e4936294b123e74722f8089d8eff30
Sylvain Nicolet <nicoletsylvain@gmail.com>
2024-04-21 17:05:58 +0200

    Implement auth callback, role user

--- auth.ts
+++ auth.ts
@@ -3,8 +3,41 @@ import authConfig from '@/auth.config'
 
 import { db } from '@/lib/db'
 import { PrismaAdapter } from '@auth/prisma-adapter'
+import { getUserById } from '@/data/user'
+import { UserRole } from '@prisma/client'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  callbacks: {
+    async signIn({ user }) {
+      if (!user.id) return false
+
+      const existingUser = await getUserById(user.id)
+
+      return !(!existingUser || !existingUser.emailVerified)
+    },
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole
+      }
+
+      return session
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token
+
+      const existingUser = await getUserById(token.sub)
+
+      if (!existingUser) return token
+
+      token.role = existingUser.role
+
+      return token
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
